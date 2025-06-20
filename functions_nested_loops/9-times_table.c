4e38a9a7646710314936ed3fb85077e5fba546a9
h7k2 <11447@holbertonstudents.com>
2025-06-20 19:40:19 +0000

    .

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int i, j, prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+		prod = i * j;
+
+	if (j == 0)
+		{
+		_putchar('0' + prod);
+          	}
+	else if (prod < 10)
+		{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar('0' + prod);
+		}
+	else
+		{
+		_putchar(',');
+		_putchar(' ');
+		_putchar('0' + (prod / 10));
+		_putchar('0' + (prod % 10));
+		}
+	}
+	_putchar('\n');
+	}
+}
