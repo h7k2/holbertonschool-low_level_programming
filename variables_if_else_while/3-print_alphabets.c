f98497689c84c1b442fc34c7cccd35c01c5508a1
root <11447@holbertonstudents.com>
2025-06-20 07:56:56 +0000

    test 3.1

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,16 @@ int main(void)
 {
    char c;
 	{
-    for (c = 'a'; c <= 'z'; c++)
-        putchar(c);      
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);      
 	}
 	{
-    for (c = 'A'; c <= 'Z'; c++)
-        putchar(c);    
+	for (c = 'A'; c <= 'Z'; c++)
+	putchar(c);    
 	}
 	{
-    putchar('\n');         
+	putchar('\n');         
 	}
-    return (0);
+	return (0);
 }
 
