2ffb8b7974a29436f6f12a0772acf421cfc1c726
root <11447@holbertonstudents.com>
2025-06-20 07:53:30 +0000

    connexion

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase and uppercase alphabet followed by a newline
+ * Return: Always 0
+ */
+int main(void)
+{
+   char c;
+	{
+    for (c = 'a'; c <= 'z'; c++)
+        putchar(c);      
+	}
+	{
+    for (c = 'A'; c <= 'Z'; c++)
+        putchar(c);    
+	}
+	{
+    putchar('\n');         
+	}
+    return (0);
+}
+
