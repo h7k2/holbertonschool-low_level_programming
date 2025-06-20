fa0199fb3762fe4b948727de59efce5289510eb9
root <11447@holbertonstudents.com>
2025-06-20 08:29:56 +0000

    5.1

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - show numbers from 0 to 9
+ * Return: 0 
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+
+	{
+	putchar(c);
+	c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}	
