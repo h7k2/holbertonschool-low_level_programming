ed94378e63653d68e635d05ba8d24e5b46e83f1d
h7k2 <11447@holbertonstudents.com>
2025-06-25 08:58:50 +0000

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@
 int main (void)
 {
 int i;
-	for ( i = 1; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i > 1)
 			printf(" ");
@@ -18,8 +18,8 @@ int i;
 		else if (i % 5 == 0)
 			printf("Buzz");
 		else if (i % 3 == 0)
-			printf("Fizz"); 
-		else 
+			printf("Fizz");
+		else
 			printf("%d", i);
 	}
 	printf("\n");
