4b0ae148cb1b87239521f914d2164185bfb44117
h7k2 <11447@holbertonstudents.com>
2025-06-25 08:57:29 +0000

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,23 @@
-#include<stdio.h> 
+#include<stdio.h>
 /**
  * FizzBuzz project
  * 3 = Fizz
  * 5 = Buzz
  * 3 & 5 = FizzBuzz 
- * */
-int main(void)
+ */
+int main (void)
 {
-int i; 
-	for( i = 1; i <= 100; i++)
+int i;
+	for ( i = 1; i <= 100; i++)
 	{
-		if(i > 1)
+		if (i > 1)
 			printf(" ");
 
-		if(i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		else if(i % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz");
-		else if(i % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz"); 
 		else 
 			printf("%d", i);
