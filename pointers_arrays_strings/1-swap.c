0905fff826715d0e20b39529e8e66cdb925d65e0
h7k2 <11447@holbertonstudents.com>
2025-06-27 12:18:56 +0000

    swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,10 +1,3 @@
-/**
- * swap_int - échange les valeurs de deux entiers
- * @a: pointeur vers le premier entier
- * @b: pointeur vers le second entier
- *
- * Retourne: rien (void)
- */
 void swap_int(int *a, int *b)
 {
     int temp;
