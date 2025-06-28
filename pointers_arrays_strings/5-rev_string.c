08778f65875078285279c52deba3888db672a96e
h7k2 <11447@holbertonstudents.com>
2025-06-28 09:43:32 +0000

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string to reverse
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (s[i] != '\0')
+  {
+		i++;
+  }
+	i--; /* Move i to the last character (not the null terminator) */
+
+	while (j < i)
+  {
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+		j++;
+		i--;
+  }
+}
