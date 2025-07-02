f46e7dd73d4cb147a4941ce6b3502a8cb2a218ad
h7k2 <11447@holbertonstudents.com>
2025-07-02 11:11:55 +0000

    fix 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,21 @@
-void remplirMemoire(void *ptr, char valeur, int taille) {
-    char *p = (char *)ptr;
-    for (int i = 0; i < taille; i++) {
-        p[i] = valeur;
-    }
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to the memory area to fill
+ * @b: the byte to fill the memory with
+ * @n: the number of bytes to fill
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
