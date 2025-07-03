cb40edd51f3e3b68c28e004d3158ff786e9089c8
h7k2 <11447@holbertonstudents.com>
2025-07-03 18:01:00 +0000

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-        printf("Error\n");
+	printf("Error\n");
 	return (1);
 	}
 
@@ -24,5 +24,5 @@ int main(int argc, char *argv[])
 
 	printf("%d\n", result);
 
-	return (0);
+		return (0);
 }
