8e37e33a2e8fc288bd705ca22499539cf189b116
h7k2 <11447@holbertonstudents.com>
2025-07-10 21:21:20 +0000

    0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to the allocated memory
+ * If malloc fails, the process exits with status code 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
