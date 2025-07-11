620a338436db4f22da4ea0479b911aecd4a2463d
h7k2 <11447@holbertonstudents.com>
2025-07-11 10:13:10 +0000

    0

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
@@ -33,6 +36,5 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void *malloc_checked(unsigned int b);
 
 #endif
