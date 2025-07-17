cdf2e4e46daa1d2a1213f8a02dac1ce2d32daa18
h7k2 <11447@holbertonstudents.com>
2025-07-17 16:00:44 +0200

    prototype

--- variadic_functions/function_pointers.h
+++ variadic_functions/function_pointers.h
@@ -7,5 +7,6 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
 int (*get_op_func(char *s))(int, int);
+int sum_them_all(const unsigned int n, ...);
 
 #endif
