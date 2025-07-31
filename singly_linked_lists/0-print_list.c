aefcbbbabf75de0c2b4b5848ad7935f39bdaaa31
h7k2 <keddousheytem@gmail.com>
2025-07-31 13:32:49 +0200

    refactor: improve print_list function documentation and structure

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,25 +1,27 @@
+
 #include "lists.h"
 
 /**
-* print_list - prints all elements of a list_t list
-* @h: pointer to the first nodes
-*
-* Return: number of nodes
-*/
+ * print_list - Prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes printed
+ */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	for (; h != NULL; h = h->next)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
-			printf("[%u] %s\n", h->len, h->str);
-
-		h = h->next;
+		{
+		printf("[%u] %s\n", h->len, h->str);
+		}
 		count++;
 	}
-
 	return (count);
 }
