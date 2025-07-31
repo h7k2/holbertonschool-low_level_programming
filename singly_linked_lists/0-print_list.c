943298b8d414d15e2b5faf2cf4a52a611e4a48d1
h7k2 <keddousheytem@gmail.com>
2025-07-31 09:43:25 +0200

    0 print new test

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,25 +1,26 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
+ * print_list - prints all elements of a list_t list
  * @h: pointer to the first node
  *
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
-    int i = 0;
+    size_t count = 0;
 
     while (h != NULL)
-    {
-        if (h->str != NULL)
-            printf("[%d] %s\n", h->len, h->str);
-        else
-            printf("[0] nil\n");
+	{
+        if (h->str == NULL)
+		    printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 
-        h = h->next;
-        i++;
-    }
+		h = h->next;
+		count++;
+	}
 
-    return (i);
+	return (count);
 }
