9769acc8765bb933c53eddec2bc08b34d51bcb4c
h7k2 <keddousheytem@gmail.com>
2025-07-31 14:50:47 +0200

    lists fixed

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -8,34 +8,29 @@
 #include <unistd.h>
 
 /**
- * struct list_s - singly linked list node structure
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
  * @next: points to the next node
  *
- * Description: Structure representing a node in a singly linked list
+ * Description: doubly linked list node structure
+ * 
  */
-typedef struct list_s
+typedef struct dlistint_s
 {
-	char *str;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
 
-	unsigned int len;
-
-	struct list_s *next;
-} list_t;
-
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-
-void free_list(list_t *head);
-void free_dlistint(dlistint_t *head);
 
 #endif
