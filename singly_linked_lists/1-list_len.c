c9c7bdf2a5006805d002731a4b789481be587e5e
h7k2 <keddousheytem@gmail.com>
2025-07-31 14:16:36 +0200

    1 final

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+
+#include "lists.h"
+
+/**
+ * list_len - Returns the number of elements in a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+
+size_t count = 0;
+
+for (; h != NULL; h = h->next)
+{
+	count++;
+}
+return (count);
+}
