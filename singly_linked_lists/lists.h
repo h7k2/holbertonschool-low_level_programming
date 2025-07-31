d8d2858cb8126302e14421b59b9b53a3e2f2b102
h7k2 <keddousheytem@gmail.com>
2025-07-31 09:42:57 +0200

    upgrade list.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#ifndef LISTS_H
+#ifnd#ifndef LISTS_H
 #define LISTS_H
 
 #include <stddef.h>
@@ -8,8 +8,6 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
- * Description: node structure for singly linked list
  */
 typedef struct list_s
 {
@@ -19,5 +17,9 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif /* LISTS_H */
