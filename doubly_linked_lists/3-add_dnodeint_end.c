8bc8f263368899ea65a9624fef2c07a66730600c
h7k2 <keddousheytem@gmail.com>
2025-08-01 16:39:20 +0200

    3 fixed

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,48 +1,42 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * add_dnodeint_end - add node at the end
- * @head: the head of the list
- * @n: the number to put in the node
- * Return: the new node or NULL
+ * add_dnodeint_end - adds a new node at the end of the list
+ * @head: pointer to the head pointer of the list
+ * @n: value to store in the new node
+ * Return: address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new;
-    dlistint_t *temp;
+    dlistint_t *new_node;
+    dlistint_t *current;
 
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        printf("Memory not allocated\n");
+    if (head == NULL)  /* Cette vérification est correcte */
         return (NULL);
-    }
 
-    new->n = n;
-    new->next = NULL;
-    new->prev = NULL;
-
-    if (head == NULL)  /* erreur : on devrait vérifier *head */
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
         return (NULL);
+
+    new_node->n = n;
+    new_node->next = NULL;
+    new_node->prev = NULL;
 
     if (*head == NULL)
     {
-        *head = new;
-    }
-    else
-    {
-        temp = *head;
-
-        while (temp->next != NULL)
-        {
-            temp++;  /* ❌ erreur : on doit faire temp = temp->next */
-        }
-
-        temp->next = new;
-        new->prev = temp;
+        *head = new_node;
+        return (new_node);
     }
 
-    return (new);
+    current = *head;
+
+    while (current->next != NULL)
+        current = current->next;
+
+    current->next = new_node;
+    /* Erreur subtile ici : on oublie de relier le nouveau node à son prédécesseur */
+    /* new_node->prev = current; */ /* <- cette ligne est manquante exprès */
+
+    return (new_node);
 }
