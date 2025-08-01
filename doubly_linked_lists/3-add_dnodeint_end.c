6443c25b06e0e16724e53f01000769eda360d456
h7k2 <keddousheytem@gmail.com>
2025-08-01 16:42:35 +0200

    3 fixed

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,18 +25,15 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (*head == NULL)
 	{
-		/* The list is empty, new node becomes the head */
 		new_node->prev = NULL;
 		*head = new_node;
 		return (new_node);
 	}
 
-	/* Go to the last node */
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
 
-	/* Append the new node */
 	temp->next = new_node;
 	new_node->prev = temp;
 
