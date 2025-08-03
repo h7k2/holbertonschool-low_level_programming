0f376c1bb7b460c7193474b464c7616e1c40c2e1
h7k2 <keddousheytem@gmail.com>
2025-08-03 13:39:53 +0200

    3

---
+++ file_io/3-cp.c
@@ -0,0 +1,77 @@
+
+#include "main.h"
+
+#define BUF_SIZE 1024
+
+/**
+ * print_error - prints error and exits
+ * @code: exit code
+ * @msg: message format string
+ * @arg: argument to insert in format
+ */
+void print_error(int code, const char *msg, const char *arg)
+{
+	dprintf(STDERR_FILENO, msg, arg);
+	exit(code);
+}
+
+/**
+ * copy_file - copies content from one file to another
+ * @file_from: source filename
+ * @file_to: destination filename
+ */
+void copy_file(const char *file_from, const char *file_to)
+{
+	int fd_from, fd_to, r, w;
+	char buffer[BUF_SIZE];
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+		print_error(98, "Error: Can't read from file %s\n", file_from);
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		print_error(99, "Error: Can't write to %s\n", file_to);
+	}
+
+	while ((r = read(fd_from, buffer, BUF_SIZE)) > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w != r)
+		{
+			close(fd_from);
+			close(fd_to);
+			print_error(99, "Error: Can't write to %s\n", file_to);
+		}
+	}
+	if (r == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+		print_error(98, "Error: Can't read from file %s\n", file_from);
+	}
+
+	if (close(fd_from) == -1)
+		print_error(100, "Error: Can't close fd %d\n", file_from);
+	if (close(fd_to) == -1)
+		print_error(100, "Error: Can't close fd %d\n", file_to);
+}
+
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	copy_file(argv[1], argv[2]);
+	return (0);
+}
